1ee89957804a08b9ed049a7db4ab06a688044669
Hina Chen <hinablue@gmail.com>
2025-05-26 18:15:25 +0800

    Testing custom loss type stable_mse

--- kohya_gui/class_advanced_training.py
+++ kohya_gui/class_advanced_training.py
@@ -146,7 +146,7 @@ class AdvancedTraining:
             with gr.Row():
                 self.loss_type = gr.Dropdown(
                     label="Loss type",
-                    choices=["huber", "smooth_l1", "l1", "l2", "stable_max"],
+                    choices=["huber", "smooth_l1", "l1", "l2", "stable_mse"],
                     value=self.config.get("advanced.loss_type", "l2"),
                     info="The type of loss to use and whether it's scheduled based on the timestep",
                 )
